cd395222cbf85695c0f1846e5506c13f3f042c78
siddhant-bhardwaj <bhardwajsiddhant2005@gmail.com>
2020-04-04 10:47:47 +0530

    Testing git

--- sketch.js
+++ sketch.js
@@ -27,9 +27,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
     log3 =  new Log(810,180,300, PI/2);
-
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
